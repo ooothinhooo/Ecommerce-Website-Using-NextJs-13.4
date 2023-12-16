183b25ad1a3795a8576b93803124ed67b241453a
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2023-12-16 17:06:45 +0700

    first commit

---
+++ app/api/register/route.ts
@@ -0,0 +1,25 @@
+import prisma from "@/app/prismadm"
+import bcrypt from "bcrypt"
+import { NextResponse } from "next/server"
+
+
+export async function POST(request: Request){
+    const body = await request.json()
+    const { name, email, password}=body
+
+
+    const hashedPassword = await bcrypt.hash(password,12)
+
+    try {
+        const user = await prisma.user.create({
+            data:{
+                name,
+                email,
+                password :hashedPassword
+            }
+        })
+        return NextResponse.json(user)
+    } catch (error) {
+        return NextResponse.error()
+    }
+}
---
+++ app/components/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignForm from './SignForm'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <SignForm/>
+  )
+}
+
+export default page
